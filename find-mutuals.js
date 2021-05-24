3ca04b66d40a2ac863fb75f911ecba2eb3bd118a
Brian Hall <briandavidhall@gmail.com>
2021-05-24 17:56:49 -0500

    start paging

--- find-mutuals.js
+++ find-mutuals.js
@@ -15,6 +15,44 @@ const USERNAME = 'briandavidhall';
 
 const user = await twitterClient.v2.userByUsername(USERNAME);
 
-const followers = await twitterClient.v2.followers(user.data.id);
+const userID = user.data.id;
 
-console.log(followers.data);
+//const followers = getFollowers(userID);
+
+
+//let followers = await twitterClient.v2.followers(userID);
+let followers = await getFollowers(userID);
+
+async function getFollowers(userID) {
+  let followers = await twitterClient.v2.followers(userID);
+  let next = followers.meta.next_token;
+  console.log(`next token is ${next}`);
+
+  let moarFollowers = await twitterClient.v2.followers(userID, {pagination_token: next});
+
+  let allFollowers = followers.data.concat(moarFollowers.data);
+  console.log(allFollowers.length);
+
+  return allFollowers; // TODO
+}
+
+// TODO set max_results: 1000 once paging solved :)
+
+/*
+const following = await twitterClient.v2.following(userID);
+
+console.log(`following count: ${following.data.length}`);
+
+let mutuals = followers.data.filter(isFollowing);
+
+console.log(`mutuals count: ${mutuals.length}`);
+
+//console.log(mutuals);
+
+function isFollowing(userData) {
+  // Given the userData (id, name, username) of a single user
+  // return true if they are contained in following
+  console.log(`checking ${JSON.stringify(userData)}`);
+  return following.data.some(followingUser => followingUser.id === userData.id);
+}
+*/
